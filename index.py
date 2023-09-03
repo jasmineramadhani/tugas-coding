d6309302493bfcb24245425bedb82e2bcd16c021
jasmineramadhani <143859893+jasmineramadhani@users.noreply.github.com>
2023-09-04 00:42:34 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,7 @@
+print ("konversi suhu farenhit ke celcius")
+
+f = float (input ("suhu farenhit : " ))
+
+h = 5/9 * (f-32)
+
+print("hasilnya :" ,h,"F")
---
+++ indexd.py
@@ -0,0 +1,7 @@
+print("mengkonversi suhu farenhit")
+
+suhu = float(input("suhu celcius  : "))
+
+h = (9/5) * suhu + 32
+
+print ("hasilnya : ",h,"C")
